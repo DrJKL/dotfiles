27eaed37288641e8c0de2b5d7f41c15348afc57a
DrJKL <DrJKL0424@gmail.com>
2017-02-03 10:04:36 -0800

    setup symlinks script

---
+++ symlinks.sh
@@ -0,0 +1,5 @@
+ln -s ~/dotfiles/i3status ~/.i3status.conf
+ln -s ~/dotfiles/i3config ~/.i3/config
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/Xresources ~/.Xresources
+ln -s ~/dotfiles/xinitrc ~/.xinitrc
